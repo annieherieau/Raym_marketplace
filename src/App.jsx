5dc5dd3de39391320e260edc30904d357f220476
Ton nom d'utilisateur githubYannRZG <yann.rezigui@outlook.fr>
2024-06-17 16:42:44 +0200

    cart

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import { useEffect, useState } from "react";
 import { unknownUser, userAtom } from "./app/atoms";
 import { loadCookie } from "./app/utils";
 import { CartProvider } from "./components/CartContext";
+import Cart from "./components/Cart";
 
 const basename = import.meta.env.VITE_BASENAME;
 const api_url = import.meta.env.VITE_BACK_API_URL;
@@ -54,6 +55,7 @@ function App() {
             <Route path="/password/:action" element={<Password />} />
             <Route path="/user_settings" element={<UserSettings />} />
             <Route path="*" element={<NotFound />} />
+            <Route path="/cart" element={<Cart />} />
           </Routes>
         </CartProvider>
       </main>

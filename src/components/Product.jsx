ea8cdc3b87b98fb5f2ffa16065eb75e34868f9ac
Korblen <malo.bastianelli@gmail.com>
2024-06-19 15:44:00 +0200

    CRUD product

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,21 +2,22 @@ import { useCart } from '../components/CartContext';
 import PropTypes from 'prop-types';
 import { buildRequestOptions } from '../app/api';
 import { useAtomValue } from "jotai";
-import { userAtom } from "../app/atoms";
+import { userAtom, isAuthAtom } from "../app/atoms";
+import { useNavigate } from 'react-router-dom';
 
-const Product = ({ product }) => {
+const Product = ({ product, onUpdateProduct, onDeleteProduct }) => {
   const { dispatch } = useCart();
-  const { token } = useAtomValue(userAtom);
+  const { token, isAdmin } = useAtomValue(userAtom);
+  const isLoggedIn = useAtomValue(isAuthAtom);
+  const navigate = useNavigate();
 
   const handleAddToCart = () => {
     console.log(product);
 
-    const { url, options } = buildRequestOptions('cart_items', 'create', 
-      {
+    const { url, options } = buildRequestOptions('cart_items', 'create', {
       body: { product_id: product.id, quantity: 1 },
       token: token
-    }
-    );
+    });
     console.log(url, options);
     fetch(url, options)
       .then(response => {
@@ -30,11 +31,27 @@ const Product = ({ product }) => {
       .catch(error => console.error('Error:', error));
   };
 
+  const handleUpdateClick = () => {
+    navigate(`/products/${product.id}/edit`); // Navigue vers la page d'édition
+  };
+
+  const handleDeleteClick = () => {
+    onDeleteProduct(product.id);
+  };
+
   return (
     <div>
       <h2>{product.name}</h2>
       <p>{product.description}</p>
-      <button onClick={handleAddToCart}>Add to Cart</button>
+      {isLoggedIn && !isAdmin && (
+        <button onClick={handleAddToCart}>Add to Cart</button>
+      )}
+      {isAdmin && (
+        <div>
+          <button onClick={handleUpdateClick}>Edit</button>
+          <button onClick={handleDeleteClick}>Delete</button>
+        </div>
+      )}
     </div>
   );
 };
@@ -45,6 +62,8 @@ Product.propTypes = {
     description: PropTypes.string,
     id: PropTypes.number.isRequired,
   }).isRequired,
+  onUpdateProduct: PropTypes.func,
+  onDeleteProduct: PropTypes.func,
 };
 
 export default Product;

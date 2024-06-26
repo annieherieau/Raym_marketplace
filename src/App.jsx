86871db974a5aea315191e9b10d668d68f4a7261
Ton nom d'utilisateur githubYannRZG <yann.rezigui@outlook.fr>
2024-06-26 17:48:28 +0200

    maintenance

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,7 @@ import Configurator from "./pages/Configurator/Configurator";
 import { useState } from "react";
 import { loadCookie } from "./app/utils";
 import { unknownUser } from "./app/atoms";
+import CreateProduct from "./components/CreateProduct";
 
 
 const api_url = import.meta.env.VITE_BACK_API_URL;

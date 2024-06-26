16e3260f676a7cf10f4a74637245f885a1ccc237
Robena Rasolond <82022919+Robe-Ras@users.noreply.github.com>
2024-06-26 18:38:35 +0200

    JumbotronSince

---
+++ src/components/Jumbotron/JumbotronSince.jsx
@@ -0,0 +1,16 @@
+const JumbotronSince = () => {
+  return (
+    <section className="text-gray-100 body-font bg-black ml-8 mr-8 rounded-[20px] mb-8">
+      <div className="container px-5 py-8 mx-auto">
+        <div className="flex flex-col text-center w-full">
+          <h1 className="sm:text-5xl text-4xl font-medium title-font mb-4 text-white">
+          Depuis 2024, nous redéfinissons le cyclisme : on road, off road, et hybrid. Faites confiance à notre expertise et<span className="font-bold" style={{ color: 'palegreen' }}> découvrez l'innovation à chaque tour de pédale !</span>
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default JumbotronSince;
+
